7607d628b545d67800c221a476335a4445fde683
darthRevan <srinivassibi@gmail.com>
2023-04-29 19:29:15 +0800

    added pdfExtract, moved text extracters into backend/model

---
+++ back end/model/docxExtract.js
@@ -0,0 +1,15 @@
+//extract txt frm word docs.
+var mammoth = require("mammoth");
+
+function extractDocx() {
+  mammoth
+    .extractRawText({ path: "SPIM.docx" })
+    .then(function (result) {
+      var text = result.value; // The raw text
+
+      //this prints all the data of docx file
+      console.log(text);
+      var messages = result.messages;
+    })
+    .done();
+}
