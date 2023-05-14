c48f5314786bb8b2a2a7b08c6fd300523bff1d98
darthReven <98806165+darthReven@users.noreply.github.com>
2023-05-14 19:12:21 +0800

    made social media scraper more dynamic

--- frontend/scripts/socialMediaScraper.js
+++ frontend/scripts/socialMediaScraper.js
@@ -1,17 +1,35 @@
 const fs = require("fs");
 const puppeteer = require("puppeteer");
+const pageurl = "https://twitter.com/nasa";
 
-const scrapeInfiniteScrollItems = async (page, itemTargetCount) => {
-  await new Promise((resolve) => setTimeout(resolve, 2000));
+// dictionary storing the configurations for popular websites
+var websiteConfigs = {
+  twitter: ["span.css-901oao.css-16my406.r-poiln3.r-bcqeeo.r-qvutc0", "H1"],
+  reddit: ["H1", "H2", "H3", "H4"],
+};
+
+// scraper
+const scrapeInfiniteScrollItems = async (page, configs, itemTargetCount) => {
+  await new Promise((resolve) => setTimeout(resolve, 2500));
   let items = [];
 
   while (itemTargetCount > items.length) {
-    items = await page.evaluate(() => {
-      const items = Array.from(
-        document.querySelectorAll("#id__b5wder1wfaf > span:nth-child(1)")
-      );
-      return items.map((item) => item.innerText);
-    });
+    items = await page.evaluate((configs) => {
+      const selectedItems = [];
+
+      const elements = Array.from(document.querySelectorAll("*"));
+
+      for (const element of elements) {
+        for (const selector of configs["twitter"]) {
+          if (element.matches(selector)) {
+            selectedItems.push(element);
+            break;
+          }
+        }
+      }
+
+      return selectedItems.map((item) => item.innerText);
+    }, configs);
 
     previousHeight = await page.evaluate("document.body.scrollHeight");
     await page.evaluate("window.scrollTo(0, document.body.scrollHeight)");
@@ -24,23 +42,17 @@ const scrapeInfiniteScrollItems = async (page, itemTargetCount) => {
   return items;
 };
 
+// calling the scraper
 (async () => {
   const browser = await puppeteer.launch({
     headless: false,
   });
 
   const page = await browser.newPage();
-  await page.goto("https://twitter.com/nasa");
+  await page.goto(pageurl);
 
-  const items = await scrapeInfiniteScrollItems(page, 5);
-  // const items2 = await scrapeInfiniteScrollItems(page, 10);
+  const items = await scrapeInfiniteScrollItems(page, websiteConfigs, 5);
 
   console.log(items);
-  // for (var i = 0; i < items.length; i++){
-  //   console.log(items[i])
-  // }
   browser.close();
-  // console.log(items2)
 })();
-
-//https://github.com/michaelkitas/Nodejs-Puppeteer-Tutorial/blob/master/infinite_scrolling.js
