1d1fb7e25fd7dc817bed9392e414e7c3ac01761d
darthReven <98806165+darthReven@users.noreply.github.com>
2023-07-01 19:49:52 +0800

    Update misinfo.py

--- backend/model/misinfo_model/misinfo.py
+++ backend/model/misinfo_model/misinfo.py
@@ -32,6 +32,7 @@ data.head()
 # Train-test split
 X_train, X_test, y_train, y_test = train_test_split(data['combined'], data.label, test_size=0.20, random_state=3101)
 
+# print(X_test)
 # Count Vectorizer
 count_vectorizer = CountVectorizer(ngram_range=(1, 2), stop_words='english')
 # Fit and transform the training data
@@ -40,12 +41,19 @@ count_train = count_vectorizer.fit_transform(X_train)
 count_test = count_vectorizer.transform(X_test)
 
 # Ensemble with Hyperparam Tuning
-model = LogisticRegression(random_state=3101)
+model = LogisticRegression(random_state=3101,solver='liblinear')
 model.fit(count_train, y_train)
 
 # calculating accuracy of model
-predictions = model.predict(count_test)
-from sklearn.metrics import accuracy_score
-# Calculate accuracy
-accuracy = accuracy_score(y_test, predictions)
-print("Accuracy:", accuracy)
+text="as a turk, i don't think this map is accurate. over %20 would just flee the country, no way %73 would fight for turkey. and the context is missing. i mean, what kind of war? i would never fight an islamist war; though i would fight to preserve my national identity and secular republic. same goes for others... some people would not fight for a 'secular' turkey but an islamist one. the map just makes no sense as it is."
+# text='theres already too many kids in the***system!!! removing potential adoptive parents just ensures more kids will grow up in this broken ***ty system.'
+text=count_vectorizer.transform([text])
+# predictions = model.predict(text)
+probabilities = model.predict_proba(text)
+prediction = probabilities[0][1]
+print(prediction)
+# predictions = model.predict(count_test)
+# from sklearn.metrics import accuracy_score
+# # Calculate accuracy
+# accuracy = accuracy_score(y_test, predictions)
+# print("Accuracy:", accuracy)
