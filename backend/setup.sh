8ed808a913bfb1f81226b85db57772f8f8e4c7b9
darthReven <98806165+darthReven@users.noreply.github.com>
2023-07-15 12:48:04 +0800

    chrome update

--- backend/setup.sh
+++ backend/setup.sh
@@ -1,7 +1,17 @@
-# Download the Chrome installer
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo dpkg -i google-chrome-stable_current_amd64.deb
-sudo apt-get -f install
-rm google-chrome-stable_current_amd64.deb
+# Specify the installation directory
+install_dir="./model/chrome"
 
-# install the other modules for botbuster
+# Create the installation directory if it doesn't exist
+mkdir -p $install_dir
+
+# Download the Chrome installer
+wget -P $install_dir https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+# Install Chrome using dpkg
+sudo dpkg -i $install_dir/google-chrome-stable_current_amd64.deb
+
+# Install any missing dependencies
+sudo apt-get -f install
+
+# Clean up the downloaded installer
+rm $install_dir/google-chrome-stable_current_amd64.deb
