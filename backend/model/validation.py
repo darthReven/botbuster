1bea6b9651d5f655f6aeef0eee54950116e6dfc0
darthReven <98806165+darthReven@users.noreply.github.com>
2023-08-12 19:43:42 +0800

    comprehensive elements list

--- backend/model/validation.py
+++ backend/model/validation.py
@@ -67,23 +67,23 @@ def check_elements(elements):
     element_regex = r"^(?:[a-zA-Z_][\w-]*\.)+[a-zA-Z_][\w-]*$|^([a-zA-Z_][\w-]*)$"
     # return bool(re.match(element_regex, elements))
     if(bool(re.match(element_regex, elements))==True):
-        valid_html_elements = [
-        "a", "abbr", "address", "area", "article", "aside", "audio", "b", "base", "bdi", "bdo", "blockquote", "body",
-        "br", "button", "canvas", "caption", "cite", "code", "col", "colgroup", "data", "datalist", "dd", "del",
-        "details", "dfn", "dialog", "div", "dl", "dt", "em", "embed", "fieldset", "figcaption", "figure", "footer",
-        "form", "h1", "h2", "h3", "h4", "h5", "h6", "head", "header", "hgroup", "hr", "html", "i", "iframe", "img",
-        "input", "ins", "kbd", "label", "legend", "li", "link", "main", "map", "mark", "meta", "meter", "nav",
-        "noscript", "object", "ol", "optgroup", "option", "output", "p", "param", "picture", "pre", "progress", "q",
-        "rb", "rp", "rt", "rtc", "ruby", "s", "samp", "script", "section", "select", "slot", "small", "source",
-        "span", "strong", "style", "sub", "summary", "sup", "table", "tbody", "td", "template", "textarea", "tfoot",
-        "th", "thead", "time", "title", "tr", "track", "u", "ul", "var", "video", "wbr",
-        "article", "aside", "bdi", "command", "details", "dialog", "summary", "figure", "figcaption",
-        "footer", "header", "hgroup", "mark", "meter", "nav", "output", "progress", "section", "time",
-        "acronym", "applet", "basefont", "big", "blink", "center", "dir", "font", "frame", "frameset", "isindex",
-        "noframes", "s", "strike", "tt", "u", "xmp"
+        html_elements = [
+            "a", "abbr", "acronym", "address", "area", "article", "aside", "audio", "b",
+            "base", "bdi", "bdo", "blockquote", "body", "br", "button", "canvas", "caption",
+            "cite", "code", "col", "colgroup", "data", "datalist", "dd", "del", "details",
+            "dfn", "dialog", "dir", "div", "dl", "dt", "em", "embed", "fieldset",
+            "figcaption", "figure", "footer", "form", "h1", "h2", "h3", "h4", "h5", "h6",
+            "header", "hgroup", "hr", "html", "i", "iframe", "img", "input", "ins", "kbd",
+            "label", "legend", "li", "link", "main", "map", "mark", "menu", "meta", "meter",
+            "nav", "noscript", "object", "ol", "optgroup", "option", "output", "p", "param",
+            "picture", "pre", "progress", "q", "rb", "rp", "rt", "ruby", "s", "samp",
+            "script", "section", "select", "small", "source", "span", "strong", "style",
+            "sub", "summary", "sup", "table", "tbody", "td", "template", "textarea", "tfoot",
+            "th", "thead", "time", "title", "tr", "track", "u", "ul", "var", "video",
+            "wbr", "xmp"
         ]
         element=elements.split('.')
-        for count in valid_html_elements:
+        for count in html_elements:
             if count==element[0]:
                 return True
         return False
